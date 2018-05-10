f6fdbdd4f4e04184ba47fe5bf1858a45b05a3209
Ravuri Vinod Kumar <RVinod@optionmatrix.com>
2018-05-10 12:21:23 +0530

    Initial commit

---
+++ ConsoleApplication/ConsumeEventSync.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace ConsoleApplication
+{
+    public class ConsumeEventSync
+    {
+        public void Helloworld() //Get Hello World  
+        {
+            using (var client = new WebClient()) //WebClient  
+            {
+                client.Headers.Add("Content-Type:application/json"); //Content-Type  
+                client.Headers.Add("Accept:application/json");
+                var result = client.DownloadString("http://localhost:57268/api/values"); //URI  
+                Console.WriteLine(Environment.NewLine + result);
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
---
+++ Helloworld/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Helloworld.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
